7cf404dde80aa1d9d246a33d21592d8587369062
Dennys Marcos <51341598+bardsnight@users.noreply.github.com>
2021-03-24 01:15:06 -0300

    Update join page

--- breakfast-guild-website/pages/join.js
+++ breakfast-guild-website/pages/join.js
@@ -9,7 +9,7 @@ export default function About() {
       <Layout title={title}>
         <div className="layoutChildrenContainer">
           <div className="layoutChildrenContent">
-            
+            <p>If you're a member already, use our bot commands on discord for registering.</p>
             <p>At least I'll give you our Discord link:</p>
            
               <a href="https://discord.gg/gkVyuctr6q">
